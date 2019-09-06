eecedcf090832b0b50b9b8d4aa9d999d2dee1df4
Corey Grandmaison <cgsustainability@gmail.com>
2019-09-06 19:55:32 -0400

    Setting up routing and switches

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React, { Component } from 'react'
-import HelloWorld from './components/HelloWorld'
+import React, { Component } from "react"
+import HomePage from "./components/pages/HomePage"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import EmployeeDetailsPage from "./components/pages/EmployeeDetailsPage"
 
 class App extends Component {
   render() {
-    return <HelloWorld />
+    return (
+      <>
+        <Router>
+          <switch>
+            <Route path='./components/pages/HomePage' exact component={Home} />
+            <Route path='/' EmployeeDetailsPage />
+            <Route path='' component={About} />
+          </switch>
+        </Router>
+      </>
+    )
   }
 }
 
