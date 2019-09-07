d269fd9b0fc08be43dfa7eb0d8ab05c89e24ec23
Corey Grandmaison <cgsustainability@gmail.com>
2019-09-07 17:56:37 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React, { Component } from "react"
-import HomePage from "./components/pages/HomePage"
+import HomePage from "./pages/HomePage"
+import EmployeeDetailsPage from "./pages/EmployeeDetailsPage"
+import About from "./pages/About"
+import EmployeeListPage from "./pages/EmployeeListPage"
+
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
-import EmployeeDetailsPage from "./components/pages/EmployeeDetailsPage"
+import EmployeeDetailsPage from "./pages/EmployeeDetailsPage"
 
 class App extends Component {
   render() {
     return (
       <>
         <Router>
-          <switch>
-            <Route path='./components/pages/HomePage' exact component={Home} />
-            <Route path='/' EmployeeDetailsPage />
-            <Route path='' component={About} />
-          </switch>
+          <Switch>
+
+            <Route path="/pages/HomePage" exact component={Home} />
+
+            <Route path='/pages/About' component={About} />
+
+            <Route path='/pages/EmployeeListPage' component={EmployeeListPage} />
+
+            <Route path='/pages/EmployeeDetailsPage' component={EmployeeDetailsPage}
+
+          </Switch>
         </Router>
       </>
     )
@@ -20,3 +30,5 @@ class App extends Component {
 }
 
 export default App
+
+src\pages\HomePage.js
---
+++ src/pages/HomePage.js
@@ -0,0 +1,19 @@
+import React, { Component, useState, useEffect } from 'react'
+// import axios from 'axios'
+// import {BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+
+
+// const fetchData = async () => {
+//   const response = await axios.get(``)
+
+
+  render() {
+    return 
+    <>
+    <h1>Welcome to this Company home page dear kind sir! Velcome!!!</h1>
+    </>
+  }
+}
+
+export default HomePage
